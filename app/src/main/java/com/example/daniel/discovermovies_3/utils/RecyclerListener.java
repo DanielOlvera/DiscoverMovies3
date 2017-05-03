8902245cbb179954f8b3fb743889c582fc1ffa01
DanielOlvera <josue_olver@yahoo.com.mx>
2017-05-03 17:30:34 -0500

    Changed ACtion bar to white, changed cardview format, working on lazy loading and details imageview

---
+++ app/src/main/java/com/example/daniel/discovermovies_3/utils/RecyclerListener.java
@@ -0,0 +1,40 @@
+package com.example.daniel.discovermovies_3.utils;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Daniel on 5/3/17.
+ */
+
+public abstract class RecyclerListener extends RecyclerView.OnScrollListener {
+
+    LinearLayoutManager linearManager;
+
+    public RecyclerListener(LinearLayoutManager linearLayoutManager) {
+        this.linearManager = linearLayoutManager;
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        int visibleItemCount = linearManager.getChildCount();
+        int totalItemCount = linearManager.getItemCount();
+        int firstVisibleItemPosition = linearManager.findFirstVisibleItemPosition();
+
+        if (!isLoading() && !isLastPage()) {
+            if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
+                    && firstVisibleItemPosition >= 0) {
+                loadMoreItems();
+            }
+        }
+
+    }
+
+    protected abstract void loadMoreItems();
+
+    public abstract boolean isLastPage();
+
+    public abstract boolean isLoading();
+}
